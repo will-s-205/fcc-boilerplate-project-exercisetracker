7b31bc4a9cd9a16d9f7b1a6562fef0d8156b78db
William S <will.step.205@gmail.com>
2023-05-29 12:53:45 -0700

    user and id added to object on response

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ app.post("/api/users", async (req, res) => {
   // res.json({ postUserName, userId });
 
   const createAndSaveDocument = async (postUserName) => {
+    // find user in DB
     const isUserExist = await UserData.findOne({ username: postUserName });
+    // if user doesn't exist then insert new
     if (isUserExist == null) {
       try {
         console.log("Inserting new User into database: " + postUserName);
@@ -49,10 +51,14 @@ app.post("/api/users", async (req, res) => {
           username: postUserName
         })
         userData.save();
-        return res.json({userData});
+        const username = userData.username;
+        const _id = userData._id;
+        return res.json({username, _id});
+        // return res.json({userData});
       } catch (error) {
         console.log(error.message);
       }
+    // if user exist then show a
     } else {
       console.log("User is already exist in database");
     }
@@ -62,3 +68,38 @@ app.post("/api/users", async (req, res) => {
 
 
 })
+
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  // const postUserId = req.body._id;
+  // const userId = " "
+  let userId = req.body._id;
+  console.log(userId);
+
+  const createAndSaveDocument = async (postUserName) => {
+    const isIdExist = await UserData.findOne({ "_id": req.params._id });
+    console.log(isIdExist);
+
+    // if (isUserExist == null) {
+    //   try {
+    //     console.log("Inserting new User into database: " + postUserName);
+    //     const userData = new UserData({
+    //       username: postUserName
+    //     })
+    //     userData.save();
+    //     return res.json({userData});
+    //   } catch (error) {
+    //     console.log(error.message);
+    //   }
+    // } else {
+    //   console.log("User is already exist in database");
+    // }
+  }
+
+  // createAndSaveDocument(postUserName);
+
+
+})
+
+// GET user's exercise log: GET /api/users/:_id/logs?[from][&to][&limit]
+// Get Query Parameter Input from the Client
+// https://www.freecodecamp.org/learn/back-end-development-and-apis/basic-node-and-express/get-query-parameter-input-from-the-client
